5ce901bf0e46a1ba6a15e793ef657794c514438f
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2024-11-21 13:09:33 -0600

    estructura y poco diseño header general

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,35 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/4f6d7f0454.js" crossorigin="anonymous"></script>
     <title>Document</title>
 
     <link rel="stylesheet" type="text/css" href="css/menuOpcionesEstilos.css">
 
 </head>
 <body>
-    <header>
-        <div id="containter-logo">
-            <img id="imgLogo"src="images/logo_sin.png" alt="Logo">
-        </div>
-        <div id="container-texto">
-            <p class="textoLogo">Direccion General de Inspeccion y</p>
-            <p class="textoLogo">Vigilancia de Obras y Acciones</p>
-        </div>
-        <div id="container-datos">
-            <div id="cont-message">
-
-            </div>
-            <div id="cont-notif"> 
-
-            </div>
-            <div id="cont-infPer">
-                <p id="text-infPers">Diego Escamilla - DICO</p>
-            </div>
-            <div id="cont-perfil">
-                <img src="images/perfil_sin.png" alt="Perfil">
+<header class="header">
+        <div class="logo-container">
+            <img src="images/logo_sin.png" alt="Logo" class="logo">
+            <div class="header-text">
+                <p class="header-line">Dirección General de Inspección y</p>
+                <p class="header-line">Vigilancia de Obras y Acciones</p>
             </div>
         </div>
+        <div class="derecha-header">
+            <div class="cont-MensIcon">
+            <i class="fa-solid fa-message fa-2xl"></i>  
+            </div>
+            <div class="cont-notifIcon">
+            <i class="fa-solid fa-bell fa-2xl" style="color: #ffffff;"></i>
+            </div>
+            <div class="cont-datosHeader">
 
+            </div>
+            <div class="cont-fotoPerfil">
+                <img src="images/perfil_sin.png" alt="Perfil" class="fotoPerfil">
+            </div>
+        </div>
     </header>
     <aside>
 <h1>aside</h1>
